e16fd6d69d4e7f2d87f53359ff47b5915b8a39e5
Chetan Chinchulkar <76626599+Chetan-Chinchulkar@users.noreply.github.com>
2021-12-12 00:19:38 +0530

    hover added,

--- app.js
+++ app.js
@@ -14,13 +14,21 @@ app.use(express.urlencoded());
 
 app.get('/', (req, res) => {
     const defaultMessage = 
-    'You have reached so far!\n\nGo to instagram handle @iitgwt and type out your answer';
+    'What you see is not there and what\'s there conceals itself beneath a hand, eyes will reveal the object but tolerance must be used to unveil the objective. You might have to rack your brains but will find nothing there, may be the answer is close at hand. Don’t spend too much time looking for something at one place start exploring and you will find it right in front of your eyes.';
     res.render('index', {modalContent: defaultMessage, isWrong: 0});
 });
 
+
+app.get('/extra', (req, res) => {
+    const defaultMessage = 
+    'What you see is not there and what\'s there conceals itself beneath a hand, eyes will reveal the object but tolerance must be used to unveil the objective. You might have to rack your brains but will find nothing there, may be the answer is close at hand. Don’t spend too much time looking for something at one place start exploring and you will find it right in front of your eyes.';
+    res.render('extra');
+});
+
+
 app.post('/', (req, res) => {
     const defaultMessage = 
-    'You have reached so far!\n\nGo to instagram handle @iitgwt and type out your answer';
+    'What you see is not there and what\'s there conceals itself beneath a hand, eyes will reveal the object but tolerance must be used to unveil the objective. You might have to rack your brains but will find nothing there, may be the answer is close at hand. Don’t spend too much time looking for something at one place start exploring and you will find it right in front of your eyes.';
     var answer = req.body.answer;
     answer = answer.toLowerCase();
     if(answer == "professor"){
@@ -30,6 +38,7 @@ app.post('/', (req, res) => {
         res.render('index', {modalContent: defaultMessage, isWrong: 1});
     }
 });
+
 
 const server = app.listen(3000 || process.env.PORT, () => {
     console.log('Listening to requests on port: ' + server.address().address + server.address().port);
